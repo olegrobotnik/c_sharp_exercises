0bdd38a84951b6116bfa2e905075d93dbbabae87
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-05 12:42:56 +0300

    Added solved task_02

---
+++ task_02_bigger_smaller_numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_02_bigger_smaller_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Enter the first number: ");
+            int nmbr1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            int nmbr2 = int.Parse(Console.ReadLine());
+
+            if (nmbr1 > nmbr2)
+            {
+                Console.WriteLine($"{nmbr1} > {nmbr2}");
+            }
+            else
+            {
+                Console.WriteLine($"{nmbr1} < {nmbr2}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
