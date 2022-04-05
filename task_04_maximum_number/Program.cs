90fa3deb55a770f5d27c8a33e8bbe7650ebf0fbf
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-05 19:54:35 +0300

    Added solved task_04

---
+++ task_04_maximum_number/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти максимальное из трех чисел
+
+Random rndm = new Random();
+
+int[] arry = new int[3];
+int i = 0;
+
+while (i < arry.Length)
+{
+    arry[i] = rndm.Next(-2147483648, 2147483647);
+    i++;
+}
+
+int maxNmbr = arry.Max();
+
+for (int j = arry.GetLowerBound(0); j <= arry.GetUpperBound(0); j++)
+{
+    Console.WriteLine("   [{0,2}]: {1}", j, arry[j]);
+}
+
+Console.WriteLine();
+
+Console.WriteLine("The maximum is {0}", maxNmbr);
+Console.ReadKey();
