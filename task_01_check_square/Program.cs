4d8da8850bcb44e4b1fdc32361343947d3c02123
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-05 12:02:02 +0300

    Added solved task_01

---
+++ task_01_check_square/Program.cs
@@ -0,0 +1,32 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_01_check_square
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("Enter the first number: ");
+            double numOne = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            double numTwo = Convert.ToDouble(Console.ReadLine());
+
+            if (numOne == Math.Pow(numTwo, 2))
+            {
+                Console.WriteLine(numOne + " is the square of the " + numTwo);
+            }
+            else
+            {
+                Console.WriteLine("It's not a square of " + numTwo);
+            }
+        }
+    }
+}
