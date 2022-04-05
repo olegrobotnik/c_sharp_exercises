0b9c9ff9a24b813496c4edf236573706bf9fcc38
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-05 11:56:05 +0300

    Add project files.

---
+++ task_00_squared_number/Program.cs
@@ -0,0 +1,25 @@
+﻿// Вывести квадрат числа
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_00
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Clear();
+
+            Console.Write("Enter a number: ");
+            double inpt = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The squared number: " + Math.Round(Math.Pow(inpt, 2), 4));
+
+            Console.ReadLine();
+        }
+    }
+}
