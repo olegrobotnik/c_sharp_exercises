be7c1b9db6bd1361d06a3a916823562b3c1b39a0
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-05 19:22:42 +0300

    Added solved task_03

---
+++ task_03_show_weekday/Program.cs
@@ -0,0 +1,24 @@
+﻿// По заданному номеру дня недели вывести его название
+
+string[] weekdays = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+int nmbr = GetIntNmbr("Enter a weekday number: ", (1, 7));
+
+Console.WriteLine(weekdays[nmbr - 1]);
+Console.ReadKey();
+
+int GetIntNmbr(string msg, (int, int) range)
+{
+    int outpt = 0;
+    bool rslt = false;
+
+    while (rslt == false)
+    {
+        Console.Write(msg);
+        string? inpt = Console.ReadLine();
+        if (int.TryParse(inpt, out outpt))
+        {
+            rslt = outpt >= range.Item1 && outpt <= range.Item2;
+        }
+    }
+    return outpt;
+}
