d2669a7521b72bf5476648960da6e78803e05d4a
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-06 13:35:10 +0300

    Added solved task_07

---
+++ task_07_numbers_-N_to_N/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать числа от -N до N
+
+int N = GetIntNmbr("Enter a number: ");
+int count = 0;
+
+while (count <= 2 * N)
+{
+    Console.WriteLine("{0, 3}   | {1, 5}", count - N, N - count);
+    count++;
+}
+
+int GetIntNmbr(string msg)
+{
+    int outpt = 0;
+    bool smthngOrNmbr = false;
+    while (smthngOrNmbr == false)
+    {
+        Console.Write(msg);
+        smthngOrNmbr = int.TryParse(Console.ReadLine(), out outpt);
+    }
+    return Math.Abs(outpt);
+}
+
+Console.ReadKey();
