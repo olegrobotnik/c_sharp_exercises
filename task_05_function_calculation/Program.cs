9839c38e282d64cf7ef13adcd7714e41b5bf696a
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-06 07:46:40 +0300

    Added solved task_05

---
+++ task_05_function_calculation/Program.cs
@@ -0,0 +1,40 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+// y = x * x - 5 
+
+int nmbr = 0;
+string msg = "Enter a number: ";
+
+double x = GetIntNmbr(msg);
+double y = FuncCalc(x);
+
+Console.WriteLine($"\ny = x * x - 5 = { y}");
+Console.ReadKey();
+
+double FuncCalc(double x)
+{
+    double y = Math.Pow(x, 2) - 5;
+    if (x == 0)
+    {
+        return y;
+    }
+    else if (x < 0)
+    {
+        x = Math.Abs(x);
+        return y;
+    }
+    else
+    {
+        return y;
+    }
+}
+
+int GetIntNmbr(string msg)
+{
+    bool smthngOrNmbr = false;
+    while (smthngOrNmbr == false)
+    {
+        Console.Write(msg);
+        smthngOrNmbr = int.TryParse(Console.ReadLine(), out nmbr);
+    }
+    return nmbr;
+}
