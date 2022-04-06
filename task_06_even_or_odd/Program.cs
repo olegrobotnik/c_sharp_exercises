9168b2cb598ed1b3de49cb3fcef757fb78a66415
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-06 08:34:39 +0300

    Added solved task_06

---
+++ task_06_even_or_odd/Program.cs
@@ -0,0 +1,26 @@
+﻿// Выяснить является ли число чётным
+
+string msg = "Enter the number: ";
+int nmbr = GetIntNmbr(msg);
+
+EvenOdd(nmbr);
+
+int GetIntNmbr(string msg)
+{
+    int outpt = 0;
+    bool smthngOrNmbr = false;
+    while (smthngOrNmbr == false)
+    {
+        Console.Write(msg);
+        smthngOrNmbr = int.TryParse(Console.ReadLine(), out outpt);
+    }
+    return outpt;
+}
+
+void EvenOdd(int nmbr)
+{
+    if (nmbr % 2 == 0)
+        Console.WriteLine("\n{0} is even", nmbr);
+    else
+        Console.WriteLine("\n{0} is odd", nmbr);
+}
