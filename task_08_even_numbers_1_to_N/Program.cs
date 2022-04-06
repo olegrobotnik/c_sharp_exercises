7f84d99cf84ede3a3ce31b7b3aea4cca9f6acc5c
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-06 14:35:13 +0300

    Added solved task_08

---
+++ task_08_even_numbers_1_to_N/Program.cs
@@ -0,0 +1,32 @@
+﻿// Показать четные числа от 1 до N
+
+int N = GetIntNmbr("Enter a number: ");
+int count = 1;
+
+while (count <= N)
+{
+    if (count % 2 == 0)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}
+count++;
+
+int GetIntNmbr(string msg)
+{
+    int outpt = 0;
+    bool smthngOrNmbr = false;
+    while (smthngOrNmbr == false)
+    {
+        Console.Write(msg);
+        smthngOrNmbr = int.TryParse(Console.ReadLine(), out outpt);
+    }
+    return Math.Abs(outpt);
+}
+
+Console.ReadKey();
