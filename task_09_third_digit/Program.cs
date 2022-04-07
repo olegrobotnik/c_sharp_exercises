05aa004bcc60bc09915dc3a936c6957db8e56c09
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-07 16:58:30 +0300

    Added solved task_09

---
+++ task_09_third_digit/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int nmbr = GetIntNmbr("Enter a three-digit number: ", (100, 999));
+
+int thrdDig = nmbr % 10;
+
+Console.WriteLine($"\nThird digit is {thrdDig}");
+Console.ReadKey();
+
+int GetIntNmbr(string msg, (int?, int?) rng)
+{
+    int outpt = 0;
+    bool smthngOrNmbr = false;
+    while (smthngOrNmbr == false)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out outpt))
+        {
+            smthngOrNmbr = rng.Item1 != null && outpt >= rng.Item1 && rng.Item1 != null && outpt <= rng.Item2;
+        }
+    }
+    return Math.Abs(outpt);
+}
