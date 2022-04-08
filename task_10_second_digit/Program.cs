7eb5fd3f06c4b56a79cbe1e0f0a9dabb6199ffaf
Oleg Mukhin <oleg.robotnik@gmail.com>
2022-04-08 12:45:37 +0300

    Added solved task_10

---
+++ task_10_second_digit/Program.cs
@@ -0,0 +1,28 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int nmbr = GetIntNmbr("Enter a three-digit number: ", (100, 999));
+int dig = SlctdDig(nmbr, 2);
+Console.WriteLine($"\nThe second digit is {dig}");
+Console.ReadKey();
+
+int SlctdDig(int nmbr, int pos)
+{
+    int tmp = nmbr / (int)Math.Pow(10, pos - 1);
+    int slctdDig = tmp % 10;
+    return slctdDig;
+}
+
+int GetIntNmbr(string msg, (int?, int?) rng)
+{
+    int outpt = 0;
+    bool smthngOrNmbr = false;
+    while (smthngOrNmbr == false)
+    {
+        Console.Write(msg);
+        if (int.TryParse(Console.ReadLine(), out outpt))
+        {
+            smthngOrNmbr = outpt >= rng.Item1 && outpt <= rng.Item2;
+        }
+    }
+    return Math.Abs(outpt);
+}
